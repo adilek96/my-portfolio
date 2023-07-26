97584f43157950c1360643e7ef05e6a574c63d86
Adilek96 <ashurovadil96@gmail.com>
2023-07-26 21:18:06 +0400

    some changes

--- src/ContentEdu.js
+++ src/ContentEdu.js
@@ -24,6 +24,16 @@ export default function ContentEdu() {
       <div className="edu_wrapper">
         <div className="edu_wrapper-content">
           <img src="../img/sertificat-web.jpeg" />
+          <p>
+            <strong>This certificate </strong> confirms that user{" "}
+            <strong>Adil Ashurov </strong>
+            has <mark>successfully completed</mark>{" "}
+            <strong>WEB-developer course</strong> 13.07.2023 from teacher Ivan
+            Petrichenko on Udemy. The certificate confirms that based on the
+            data provided by the student, the course was successfully completed.
+            The course duration includes the total number of hours of video in
+            the course as of the most recent course completion.
+          </p>
         </div>
       </div>
     </motion.div>
