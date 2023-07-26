c478660a051844462edf2811d468613380eb422c
Adilek96 <ashurovadil96@gmail.com>
2023-07-26 22:55:50 +0400

    adding slider

--- src/ContentPortfolio.js
+++ src/ContentPortfolio.js
@@ -1,14 +1,35 @@
 import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 export default function ContentPortfolio() {
   return (
     <div className="content__portfolio">
-      <iframe
-        src="https://coffee-store-amber.vercel.app/"
-        width="80%"
-        height="315"
-        overflow="hidden"
-      ></iframe>
+      <h2>My Works</h2>
+      <Splide
+        options={{
+          type: "loop",
+          perPage: 3,
+          focus: "center",
+
+          breakpoints: {
+            920: {
+              perPage: 1,
+            },
+          },
+        }}
+        aria-label="My Works"
+      >
+        <SplideSlide className="slide">
+          <img src="../img/employersApp.png" alt="employersApp" />
+        </SplideSlide>
+        <SplideSlide className="slide">
+          <img src="../img/coffeShop.png" alt="coffe-shopApp" />
+        </SplideSlide>
+        <SplideSlide className="slide">
+          <img src="../img/currencyApp.png" alt="currencyApp" />
+        </SplideSlide>
+      </Splide>
     </div>
   );
 }
